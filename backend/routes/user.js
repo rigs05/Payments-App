2ddffce7da6520f8e152bfcf1d46a0a20aa27d4e
rigs05 <bosco18494@gmail.com>
2024-06-14 23:51:12 +0530

    authMiddleware, user update routes and user authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,20 +1,20 @@
-import { sign, verify } from "jsonwebtoken";
+import { sign } from "jsonwebtoken";
 import { zodSchema } from "../database/zodSchema";
 import { User } from "../database/users";
 import { Router } from "express";
 import "dotenv/config";
+import { authMiddleware } from "../middleware/authMiddleware";
 const router = Router();
 
 router.post("/signup", async (req, res) => {
   try {
     // const { fName, lName, userId, password } = req.body;
     const userDataInput = req.body;
-
-    const zodValidation = zodSchema.safeParse(userDataInput);
+    const zodValidation = await zodSchema.safeParse(userDataInput);
     if (!zodValidation.success) {
       return res.json({ error: zodValidation.data });
     }
-    let userExist = User.findOne({ userId: userDataInput.userId });
+    let userExist = await User.findOne({ userId: userDataInput.userId });
     if (userExist) {
       return res.status(411).json({
         message: "User already exist in database, please login.",
@@ -24,7 +24,7 @@ router.post("/signup", async (req, res) => {
     const token = sign({ userId: userExist.userId }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    userExist.save();
+    await userExist.save();
     res
       .status(200)
       .json({ token: token, message: "User created successfully." });
@@ -35,10 +35,51 @@ router.post("/signup", async (req, res) => {
 
 router.post("/signin", async (req, res) => {
   try {
-    res.status(200).json({});
+    const { userId, password } = req.body;
+    const userExist = await User.findOne({ userId, password });
+    if (!userExist) {
+      return res
+        .status(411)
+        .json({ message: "Error while loggin in. Check userId or password." });
+    }
+    const token = sign(
+      { userId: userId, fName: userExist.fName },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "7d",
+      }
+    );
+    res.status(200).json({ token: token });
   } catch (error) {
     throw new Error("Internal server error.");
   }
 });
+
+router.put("/", authMiddleware, async (req, res) => {
+  try {
+    const { password, fName, lName } = req.body;
+    const userId = req.userId;
+    const zodValidation = zodSchema.safeParse({
+      fName,
+      lName,
+      userId,
+      password,
+    });
+    if (!zodValidation.success) {
+      return res.status(411).json({ error: zodValidation.data });
+    }
+    const userUpdate = await User.updateOne({
+      firstName: fName,
+      lastName: lName,
+      password,
+    });
+    await userUpdate.save();
+    res.status(200).json({ message: "Information updated successfully." });
+  } catch (error) {
+    throw new Error("Internal Server Error.");
+  }
+});
+
+router.get("/bulk?filter=:name", authMiddleware, async (req, res) => {});
 
 export default router;
